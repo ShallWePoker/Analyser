bb405749b61f8020fd2ece95cb04c115e9cabe6c
Lingxiao.Luo <Lingxiao.Luo@apulisai.com>
2022-08-03 18:29:49 +0800

    feat: updated converter_h2n to read a whole directory's files into one combined file

--- cmd/converter_h2n.go
+++ cmd/converter_h2n.go
@@ -2,37 +2,118 @@ package main
 
 import (
 	"bufio"
+	"errors"
+	"flag"
+	"fmt"
 	"os"
+	"path/filepath"
 	"regexp"
+	"strings"
 )
 
 func main() {
-	singleFile, err := os.Open("/path/to/combined.txt")
+	handsTxtFolder := flag.String("hands-txt-folder", "", "hands txt folder")
+	combinedTxtPath := flag.String("combined-txt-path", "", "combined txt path")
+	playerName := flag.String("player-name", "", "player name")
+	flag.Parse()
+	fmt.Printf("hands txt folder: %s\n", *handsTxtFolder)
+	fmt.Printf("combined txt file path: %s\n", *combinedTxtPath)
+	fmt.Printf("player name: %s\n", *playerName)
+
+	allTxtFiles, err := listAllTxtFiles(*handsTxtFolder)
 	if err != nil {
 		panic(err)
 	}
-	writeFile, err := os.Create("/path/to/converted.txt")
+	_, err = os.Stat(*combinedTxtPath)
+	if err == nil {
+		fmt.Printf("combined txt file already exists. will delete it and create a new one.")
+		err = os.Remove(*combinedTxtPath)
+		if err != nil {
+			fmt.Printf("cannot delete %s.", *combinedTxtPath)
+			panic(err)
+		}
+	}
+	writeFile, err := os.Create(*combinedTxtPath)
 	if err != nil {
 		panic(err)
 	}
 	datawriter := bufio.NewWriter(writeFile)
-	defer singleFile.Close()
 	defer writeFile.Close()
-	scanner := bufio.NewScanner(singleFile)
-	reg1, err := regexp.Compile("Dealt to Hero")
-	reg2, err := regexp.Compile("Dealt to [0-9A-Za-z_]{0,}\\s+")
-	reg3, err := regexp.Compile("XXXXXXXXXXXXXXX")
+
+	pokerHandRegexp, err := regexp.Compile("(Poker Hand #)(RC|HD)")
 	if err != nil {
 		panic(err)
 	}
-
-	for scanner.Scan() {
-		replaced := reg1.ReplaceAllString(scanner.Text(), "XXXXXXXXXXXXXXX")
-		replaced = reg2.ReplaceAllString(replaced, "")
-		replaced = reg3.ReplaceAllString(replaced, "Dealt to Hero")
-		_, _ = datawriter.WriteString(replaced + "\n")
-	}
-	if err := scanner.Err(); err != nil {
+	dealtToHeroRegexp, err := regexp.Compile("Dealt to Hero")
+	if err != nil {
 		panic(err)
 	}
+	dealtToRandomRegexp, err := regexp.Compile("Dealt to [0-9A-Za-z_]{0,}\\s+")
+	if err != nil {
+		panic(err)
+	}
+	xxxxxRegexp, err := regexp.Compile("XXXXXXXXXXXXXXX")
+	if err != nil {
+		panic(err)
+	}
+	heroRegexp, err := regexp.Compile("Hero")
+	if err != nil {
+		panic(err)
+	}
+	for _, singleTxtFile := range allTxtFiles {
+		err = readFilesAppendToCombinedFile(singleTxtFile, datawriter, *playerName, pokerHandRegexp, dealtToHeroRegexp, dealtToRandomRegexp, xxxxxRegexp, heroRegexp)
+		if err != nil {
+			fmt.Printf("err dealing with txt file %s: %+v", singleTxtFile, err)
+			panic(err)
+		}
+	}
+}
+
+func listAllTxtFiles(dir string) (filePaths []string, err error) {
+	dirInfo, err := os.Stat(dir)
+	if err != nil {
+		return
+	}
+	if !dirInfo.IsDir() {
+		return filePaths, errors.New(dir + "is not a directory")
+	}
+	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() && info.Mode()&os.ModeSymlink == 0 {
+			pathParts := strings.Split(path, "/")
+			if strings.HasSuffix(path, ".txt") && strings.HasPrefix(pathParts[len(pathParts)-1], "GG") {
+				filePaths = append(filePaths, path)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return filePaths, err
+	}
+	return
+}
+
+func readFilesAppendToCombinedFile(handsTxtFile string, datawriter *bufio.Writer, playerName string, pokerHandRegexp, dealtHeroRegexp, dealtToRandomRegexp, xxxxxRegexp, heroRegexp *regexp.Regexp) error {
+	fileContent, err := os.Open(handsTxtFile)
+	if err != nil {
+		fmt.Printf("cannot open txt file %s: %+v; skipping.", handsTxtFile, err)
+		return nil
+	}
+	defer fileContent.Close()
+	scanner := bufio.NewScanner(fileContent)
+	for scanner.Scan() {
+		// TODO PokerStars Hand #20 meaning ?
+		replaced := pokerHandRegexp.ReplaceAllString(scanner.Text(), "PokerStars Hand #20")
+		replaced = dealtHeroRegexp.ReplaceAllString(replaced, "XXXXXXXXXXXXXXX")
+		replaced = dealtToRandomRegexp.ReplaceAllString(replaced, "")
+		replaced = xxxxxRegexp.ReplaceAllString(replaced, fmt.Sprintf("Dealt to %s", playerName))
+		replaced = heroRegexp.ReplaceAllString(replaced, playerName)
+		_, err = datawriter.WriteString(replaced + "\n")
+		if err != nil {
+			return err
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	return nil
 }
